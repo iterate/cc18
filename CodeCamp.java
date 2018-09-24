244e1d3a76e3217eed9545856c009c00bf771da4
oddmunds <oddmunds@iterate.no>
2018-09-24 15:37:45 +0200

    mandag

---
+++ CodeCamp.java
@@ -0,0 +1,109 @@
+package no.iterate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CodeCamp {
+
+    public static void main(String[] args) {
+        List<Testable> tests = new ArrayList<>();
+        tests.add(new IntegrationTest());
+        tests.add(new FizzBuzz());
+
+        report(runTests(tests));
+
+
+    }
+
+    public static void report(TestResults testResults) {
+        System.out.println("Tests run: " + testResults.numberOfTests);
+        System.out.println("Tests failed: " + testResults.numberOfTestsFailed);
+
+        if (testResults.numberOfTestsFailed > 0) {
+            System.exit(1);
+        }
+    }
+
+    public static TestResults runTests(List<Testable> tests) {
+        TestResults testResults = new TestResults();
+        for (Testable test : tests) {
+            try {
+                testResults.numberOfTests++;
+                test.invoke();
+            } catch (Throwable e) {
+                testResults.numberOfTestsFailed++;
+            }
+        }
+        return testResults;
+    }
+
+    private interface Testable {
+        void invoke();
+    }
+
+    private static class TestResults {
+        public int numberOfTests;
+        public int numberOfTestsFailed;
+    }
+
+    private static class FailingTest implements Testable{
+        public void invoke() {
+            throw new RuntimeException();
+        }
+    }
+
+    private static class PassingTest implements Testable{
+        public void invoke() {
+        }
+    }
+
+    private static class AssertFailedTest implements Testable {
+        public void invoke() {
+            assert(false);
+        }
+    }
+
+    private static class IntegrationTest implements Testable {
+        private static TestResults run() {
+            List<Testable> tests = new ArrayList<>();
+            tests.add(new FailingTest());
+            tests.add(new PassingTest());
+            tests.add(new AssertFailedTest());
+
+            TestResults testResults = runTests(tests);
+
+            assert(testResults.numberOfTests == 3);
+            assert(testResults.numberOfTestsFailed == 2);
+
+            return testResults;
+        }
+
+        @Override
+        public void invoke() {
+            run();
+        }
+    }
+
+    private static class FizzBuzz implements Testable {
+        public void invoke() {
+            assert(fizzBuzz(1).equals("1"));
+            assert(fizzBuzz(2).equals("2"));
+            assert(fizzBuzz(3).equals("Fizz!"));
+            assert(fizzBuzz(5).equals("Buzz!"));
+            assert(fizzBuzz(15).equals("FizzBuzz!"));
+        }
+
+        private String fizzBuzz(int i) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                return "FizzBuzz!";
+            } else if (i % 3 == 0) {
+                return "Fizz!";
+            } else if (i % 5 == 0) {
+                return "Buzz!";
+            }
+
+            return String.valueOf(i);
+        }
+
+    }
+}
