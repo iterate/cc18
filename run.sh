d22e68782cc9fbf10400127e03b1756bb9b98713
oddmunds <oddmunds@iterate.no>
2018-09-26 13:18:00 +0200

    working

--- run.sh
+++ run.sh
@@ -4,8 +4,7 @@ echo "-----"
 echo $FILES
 echo "-----"
 
-javac src/no/iterate/CodeCamp.java -d out/production/cc18/ \
-    && java -ea -classpath out/production/cc18 no.iterate.CodeCamp \
+javac src/no/iterate/CodeCamp.java -d out/production/cc18/ && java -ea -classpath out/production/cc18 no.iterate.CodeCamp \
     && (git commit -am working \
         && git push) \
     || git reset --hard
