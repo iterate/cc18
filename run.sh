383eca4a5d42dcc1118a84888d55cc522661fd7b
oddmunds <oddmunds@iterate.no>
2018-09-26 13:11:54 +0200

    working

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 FILES=$(find -name "*.java")
 javac src/no/iterate/CodeCamp.java -d out/production/cc18/ \
     && java -ea -classpath out/production/cc18 no.iterate.CodeCamp \
