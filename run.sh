70778cd4c87dcd796e47eaaa81ab3e4df8713e88
oddmunds <oddmunds@iterate.no>
2018-09-26 13:15:53 +0200

    working

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+FILES=$(ls -lrt -d -1 $PWD/**/{*,.*} | grep ".java$")
+
 javac src/no/iterate/CodeCamp.java -d out/production/cc18/ \
     && java -ea -classpath out/production/cc18 no.iterate.CodeCamp \
     && (git commit -am working \
