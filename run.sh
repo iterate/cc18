26d7fcb9b9391e99848b8b4672a446e57b8cecd6
oddmunds <oddmunds@iterate.no>
2018-09-26 13:08:03 +0200

    working

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-FILES= find -name "*.java"
+
+FILES=$(find -name "*.java")
 javac src/no/iterate/CodeCamp.java -d out/production/cc18/ \
     && java -ea -classpath out/production/cc18 no.iterate.CodeCamp \
     && (git commit -am working \
