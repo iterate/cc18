a8b6b5ed0628d62f749b90faa52ca780567fbf4e
Lars Barlindhaug <lars.barlindhaug@iterate.no>
2018-09-26 11:21:17 +0200

    working

--- no/iterate/CodeCamp.java
+++ no/iterate/CodeCamp.java
@@ -65,7 +65,7 @@ public class CodeCamp {
 
     private static class FailingTest implements Testable{
         public void invoke() {
-            throw new RuntimeException();
+            throw new RuntimeException("MyMessage");
         }
 
         @Override
