2c74c1f145e1770ecc9d5313f01788632febaaa2
Lars Barlindhaug <lars.barlindhaug@iterate.no>
2018-09-26 11:18:53 +0200

    working

--- no/iterate/CodeCamp.java
+++ no/iterate/CodeCamp.java
@@ -102,6 +102,7 @@ public class CodeCamp {
             tests.add(new FailingTest());
 
             TestResults testResults = runTests(tests);
+            System.out.println(testResults.summary());
             assert(testResults.summary().contains("RuntimeException"));
 
         }
