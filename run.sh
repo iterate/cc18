6b4b807b7e1c93b60508b7b7dcd085f08a3ef75e
oddmunds <oddmunds@iterate.no>
2018-09-26 13:21:31 +0200

    working

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env zsh
 FILES=$(ls -lrt -d -1 $PWD/**/{*,.*} | grep ".java$")
 
-echo "-----"
-echo $FILES
-echo "-----"
+echo "_________            .___     _________                       ____  ______  "
+echo "\_   ___ \  ____   __| _/____ \_   ___ \_____    _____ ______/_   |/  __  \ "
+echo "/    \  \/ /  _ \ / __ |/ __ \/    \  \/\__  \  /     \\____ \|   |>      < "
+echo '\     \___(  <_> ) /_/ \  ___/\     \____/ __ \|  Y Y  \  |_> >   /   --   \'
+echo " \______  /\____/\____ |\___  >\______  (____  /__|_|  /   __/|___\______  /"
+echo "        \/            \/    \/        \/     \/      \/|__|              \/ "
+
+
 
 javac src/no/iterate/CodeCamp.java -d out/production/cc18/ && java -ea -classpath out/production/cc18 no.iterate.CodeCamp \
     && (git commit -am working \
