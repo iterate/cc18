80ee7b5b8f976776bf34cdc26484205015e8feb7
Lars Barlindhaug <lars.barlindhaug@iterate.no>
2018-09-26 14:38:02 +0200

    working

--- src/no/iterate/Tests.java
+++ src/no/iterate/Tests.java
@@ -31,7 +31,6 @@ public class Tests {
         @Override
         public void invoke() {
             List<Testable> tests = new ArrayList<>();
-            tests.add(new CorrectAssertErrorMessage());
             tests.add(new CodeCamp.AnonymousFunction(() -> {return;}));
 
             Runnable correctErrorMessage = () -> {
