2a1e3ce06f0a6ab44a3eed786a4106313e10eeb7
Lars Barlindhaug <lars.barlindhaug@iterate.no>
2018-09-26 13:31:28 +0200

    working

--- src/no/iterate/CodeCamp.java
+++ src/no/iterate/CodeCamp.java
@@ -122,7 +122,6 @@ public class CodeCamp {
             tests.add(new AssertFailedTest());
 
             TestResults testResults = runTests(tests);
-            System.out.println(testResults.summary());
             assert(testResults.summary().contains("CodeCamp.java"));
 
         }
