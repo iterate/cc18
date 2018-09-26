bbf2fafbae2dfa3aff2e31628f6f5b2c856f09bd
Lars Barlindhaug <lars.barlindhaug@iterate.no>
2018-09-26 14:39:32 +0200

    working

--- src/no/iterate/CodeCamp.java
+++ src/no/iterate/CodeCamp.java
@@ -40,15 +40,6 @@ public class CodeCamp {
 
     public static TestResults runTests(List<Testable> tests, List<Runnable> testFunctions) {
         TestResults testResults = new TestResults();
-        for (Testable test : tests) {
-            try {
-                testResults.numberOfTests++;
-                test.invoke();
-            } catch (Throwable e) {
-                testResults.numberOfTestsFailed++;
-                testResults.exceptions.add(buildErrorMessage(e));
-            }
-        }
 
         for (Runnable test : testFunctions) {
             try {
