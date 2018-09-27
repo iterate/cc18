348cbd5d393fb4818223c792ede2f2e77485003b
Lars Barlindhaug <lars.barlindhaug@iterate.no>
2018-09-27 13:09:17 +0200

    working

--- src/no/iterate/CodeCamp.java
+++ src/no/iterate/CodeCamp.java
@@ -42,7 +42,7 @@ public class CodeCamp {
                     .addMethod("calculate")
                     .addParameter("INT", "input")
                     .toString()
-                    .contains("calculate"));
+                    .contains("calculate("));
         });
 
         report(runTests(tests, functionTests));
