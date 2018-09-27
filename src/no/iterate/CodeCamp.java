a64426cb0666f3fff0b50424490731354a74f437
Lars Barlindhaug <lars.barlindhaug@iterate.no>
2018-09-27 10:48:40 +0200

    working

--- src/no/iterate/CodeCamp.java
+++ src/no/iterate/CodeCamp.java
@@ -2,7 +2,7 @@ package no.iterate;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import com.github.javaparser.symbolsolver.JavaSymbolSolver;
 import static no.iterate.Tests.assume;
 
 public class CodeCamp {
