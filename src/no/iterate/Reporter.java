5f5974ccd5ced002bb0d718c9ea600819c06e6da
oddmunds <oddmunds@iterate.no>
2018-09-27 13:52:37 +0200

    working

--- src/no/iterate/Reporter.java
+++ src/no/iterate/Reporter.java
@@ -2,6 +2,7 @@ package no.iterate;
 
 public class Reporter {
     public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_RESET = "\u001B[0m";
     private static final String LOGO = "_________            .___     _________                       ____  ______  \n" +
             "\\_   ___ \\  ____   __| _/____ \\_   ___ \\_____    _____ ______/_   |/  __  \\ \n" +
@@ -11,6 +12,7 @@ public class Reporter {
             "        \\/            \\/    \\/        \\/     \\/      \\/|__|              \\/ ";
 
     public static void report(TestResults testResults) {
+        boolean failure = testResults.numberOfTestsFailed > 0;  
         System.out.print(ANSI_RED);
         System.out.println(LOGO);
         System.out.print(ANSI_RESET);
@@ -19,7 +21,7 @@ public class Reporter {
 
         System.out.println();
 
-        if (testResults.numberOfTestsFailed > 0) {
+        if (failure) {
             System.out.println(testResults.summary());
             System.exit(1);
         }
