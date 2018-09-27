912f4f034002234717f9539546de2ae6d9de962a
Lars Barlindhaug <lars.barlindhaug@iterate.no>
2018-09-27 13:42:49 +0200

    working

--- src/no/iterate/Program.java
+++ src/no/iterate/Program.java
@@ -4,6 +4,10 @@ import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.FieldAccessExpr;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.expr.StringLiteralExpr;
 import com.github.javaparser.ast.stmt.BlockStmt;
 
 class Program {
@@ -37,8 +41,13 @@ class Program {
     }
 
     public Program addMethodBody(String s) {
-        final BlockStmt body = new BlockStmt();
-        currentMethod.setBody(body);
+        final BlockStmt block = new BlockStmt();
+        NameExpr clazz = new NameExpr("System");
+        FieldAccessExpr field = new FieldAccessExpr(clazz, "out");
+        MethodCallExpr call = new MethodCallExpr(field, "println");
+        call.addArgument(new StringLiteralExpr("Hello World!"));
+        block.addStatement(call);
+        currentMethod.setBody(block);
         return this;
     }
 }
