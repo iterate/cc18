c2c1d07561356ac9ad730f7143c99e973637a684
oddmunds <oddmunds@iterate.no>
2018-09-27 14:16:04 +0200

    working

--- src/no/iterate/ProgramTests.java
+++ src/no/iterate/ProgramTests.java
@@ -7,4 +7,15 @@ public class ProgramTests {
             .addClass("FizzBuzz")
             .toString()
             .contains("class FizzBuzz"), "Program should contain class FizzBuzz");
+    public static final Runnable ADD_METHOD = () -> assume(new Program()
+            .addClass("FizzBuzz")
+            .addMethod("calculate")
+            .toString()
+            .contains("calculate()"), "Adding a method, 'calculate' should make THE STRING contain 'calculate()'");
+    public static final Runnable ADD_PARAMETER = () -> assume(new Program()
+            .addClass("FizzBuzz")
+            .addMethod("calculate")
+            .addParameter("INT", "input")
+            .toString()
+            .contains("calculate(INT input)"));
 }
