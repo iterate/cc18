e6baf9a0fdcd5deaa3e46bd72c83ff2d4633d14c
Lars Barlindhaug <lars.barlindhaug@iterate.no>
2018-09-27 14:35:59 +0200

    working

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-javac -classpath javaparser-symbol-solver-core-3.6.23.jar:javaparser-core-3.6.23.jar src/no/iterate/ProgramTests.java src/no/iterate/Program.java src/no/iterate/Tester.java src/no/iterate/Reporter.java src/no/iterate/Testable.java src/no/iterate/TestResults.java src/no/iterate/Tests.java src/no/iterate/CodeCamp.java -d out/production/cc18/ && \
+javac -classpath javaparser-symbol-solver-core-3.6.23.jar:javaparser-core-3.6.23.jar src/no/iterate/Compiler.java src/no/iterate/ProgramTests.java src/no/iterate/Program.java src/no/iterate/Tester.java src/no/iterate/Reporter.java src/no/iterate/Testable.java src/no/iterate/TestResults.java src/no/iterate/Tests.java src/no/iterate/CodeCamp.java -d out/production/cc18/ && \
 java -classpath out/production/cc18:javaparser-symbol-solver-core-3.6.23.jar:javaparser-core-3.6.23.jar no.iterate.CodeCamp && \
 git commit -am working || git reset --hard
 
