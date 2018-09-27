33c76fd3789baa7c5af4bebb6a8ce09da0ad6694
oddmunds <oddmunds@iterate.no>
2018-09-27 13:07:55 +0200

    working

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ function class() {
     $1
 }
 
-echo $(class "Testable")
+echo `class "Testable"`
 
 javac -classpath javaparser-symbol-solver-core-3.6.23.jar:javaparser-core-3.6.23.jar src/no/iterate/Testable.java src/no/iterate/TestResults.java src/no/iterate/Tests.java src/no/iterate/CodeCamp.java -d out/production/cc18/ && \
 java -classpath out/production/cc18:javaparser-symbol-solver-core-3.6.23.jar:javaparser-core-3.6.23.jar no.iterate.CodeCamp && \
