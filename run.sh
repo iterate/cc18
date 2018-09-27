185d700bde4086283ea656d4573ee981914f4623
oddmunds <oddmunds@iterate.no>
2018-09-27 13:05:57 +0200

    working

--- run.sh
+++ run.sh
@@ -12,6 +12,8 @@ function class() {
     echo $1
 }
 
+class "yoyo"
+
 javac -classpath javaparser-symbol-solver-core-3.6.23.jar:javaparser-core-3.6.23.jar src/no/iterate/Testable.java src/no/iterate/TestResults.java src/no/iterate/Tests.java src/no/iterate/CodeCamp.java -d out/production/cc18/ && \
 java -classpath out/production/cc18:javaparser-symbol-solver-core-3.6.23.jar:javaparser-core-3.6.23.jar no.iterate.CodeCamp && \
 git commit -am working || git reset --hard
