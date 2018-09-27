9b6e4fa77da6cc842be5176864a3509e28f8b906
oddmunds <oddmunds@iterate.no>
2018-09-27 12:49:50 +0200

    working

--- src/no/iterate/CodeCamp.java
+++ src/no/iterate/CodeCamp.java
@@ -122,7 +122,7 @@ public class CodeCamp {
         message.append("\n\nStack trace:\n");
 
         for (StackTraceElement stackTraceElement : stackTraceElements) {
-            message.append(printStackTraceMessage(stackTraceElement));
+            message.append("" + printStackTraceMessage(stackTraceElement));
         }
 
         return message.toString();
