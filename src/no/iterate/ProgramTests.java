7e9acf7d6d3edd0593ece830002e4750fac4ee4d
Lars Barlindhaug <lars.barlindhaug@iterate.no>
2018-09-28 10:02:54 +0200

    working

--- src/no/iterate/ProgramTests.java
+++ src/no/iterate/ProgramTests.java
@@ -41,7 +41,7 @@ public class ProgramTests {
             .addReturnType("STRING")
             .addMethodReturnStmt("\"1\"")
             .run()
-            .contains("return \"1\";"), "Method should return 1 like we said");
+            .contains("return \"1\";"), "Method should return \"1\" like we said");
 
 
     public static final Runnable ADD_METHOD_TO_SPECIFIC_CLASS = () -> assume(new Program()
