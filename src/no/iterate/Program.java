268c403bfeeebbcedaddf5051b962eefafc11fac
Lars Barlindhaug <lars.barlindhaug@iterate.no>
2018-09-28 12:29:27 +0200

    working

--- src/no/iterate/Program.java
+++ src/no/iterate/Program.java
@@ -53,7 +53,7 @@ class Program {
     }
 
     public Program selectMethod(String methodName) {
-        currentClass.getMethodsByName(methodName);
+        currentClass.getMethodsByName(methodName).get(0);
         return this;
     }
 
