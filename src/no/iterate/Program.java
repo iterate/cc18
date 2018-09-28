7ee082d5ed9eec56155c6daa22ef092857d34907
oddmunds <oddmunds@iterate.no>
2018-09-28 12:34:51 +0200

    working

--- src/no/iterate/Program.java
+++ src/no/iterate/Program.java
@@ -77,7 +77,6 @@ class Program {
         List<Node> nodes = compilationUnit.getChildNodes();
         for (int i = 0; i < nodes.size(); i++) {
             Node node = nodes.get(i);
-            System.out.print(i);
         }
     }
 
