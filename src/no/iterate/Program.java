56913f97308cb9d0c2166928aea5ff3c895c10c0
Lars Barlindhaug <lars.barlindhaug@iterate.no>
2018-09-28 09:53:03 +0200

    working

--- src/no/iterate/Program.java
+++ src/no/iterate/Program.java
@@ -117,7 +117,7 @@ class Program {
 
     public static Program script(String script){
         Program program = new Program();
-        script.lines().forEach(line -> program.apply(line));
+        //script.lines().forEach(line -> program.apply(line));
         return program;
     };
 
