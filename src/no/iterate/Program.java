b842ca08061fe65a952313068ed22a0e05625a7b
oddmunds <oddmunds@iterate.no>
2018-09-28 10:13:36 +0200

    working

--- src/no/iterate/Program.java
+++ src/no/iterate/Program.java
@@ -36,7 +36,7 @@ class Program {
     }
 
     public Program addMethodToClass(int node, String methodName) {
-        System.out.println( compilationUnit.getChildNodes().get(node).getClass());
+        ClassOrInterfaceDeclaration klass = (ClassOrInterfaceDeclaration) compilationUnit.getChildNodes().get(node);
         currentMethod = currentClass.addMethod(methodName);
         return this;
     }
