42e6ac573c3bdf97ae635eef18799aa858707065
Ole Schilling Tjensvold <ole@iterate.no>
2018-09-28 10:22:36 +0200

    working

--- src/no/iterate/Program.java
+++ src/no/iterate/Program.java
@@ -95,7 +95,7 @@ class Program {
         try {
             runProcess("pwd");
             System.out.println("**********");
-            runProcess("javac -classpath javaparser-symbol-solver-core-3.6.23.jar:javaparser-core-3.6.23.jar src/no/iterate/ProgramTests.java src/no/iterate/Program.java src/no/iterate/Tester.java src/no/iterate/Reporter.java src/no/iterate/Testable.java src/no/iterate/TestResults.java src/no/iterate/Tests.java src/no/iterate/CodeCamp.java -d out/production/cc18/ && \\\n");
+            runProcess("javac -classpath javaparser-symbol-solver-core-3.6.23.jar:javaparser-core-3.6.23.jar src/no/iterate/ProgramTests.java src/no/iterate/Program.java src/no/iterate/Tester.java src/no/iterate/Reporter.java src/no/iterate/Testable.java src/no/iterate/TestResults.java src/no/iterate/Tests.java src/no/iterate/FizzBuzz.java -d out/production/cc18/ && \\\n");
             System.out.println("**********");
             runProcess("java -cp FizzBuzz");
         } catch (Exception e) {
