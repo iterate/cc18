9716da3085a8e2894834ea352107f7e19a274715
Lars Barlindhaug <lars.barlindhaug@iterate.no>
2018-09-28 10:49:44 +0200

    working

--- src/no/iterate/Program.java
+++ src/no/iterate/Program.java
@@ -60,7 +60,7 @@ class Program {
 
     public Program addParameter(String parameterType, String parameterName) {
         new Parameter(JavaParser.parseTypeParameter(parameterType), parameterName);
-        currentMethod.addParameter(JavaParser.parseTypeParameter(parameterType), parameterName);
+        currentMethod.addParameter(JavaParser.parseClassOrInterfaceType(parameterType), parameterName);
         return this;
     }
 
