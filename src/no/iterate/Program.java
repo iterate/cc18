e5b91c8b6da889f164b777a630d44d71baf11661
oddmunds <oddmunds@iterate.no>
2018-09-28 13:05:54 +0200

    working

--- src/no/iterate/Program.java
+++ src/no/iterate/Program.java
@@ -82,7 +82,7 @@ class Program {
     }
 
     String indent(int depth){
-        new StringBuilder();
+        StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < depth; i++) {
             
         }
