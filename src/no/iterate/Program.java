a085795b54b26250ff3ae0977a6f4e19bfdc373c
oddmunds <oddmunds@iterate.no>
2018-09-28 11:08:09 +0200

    working

--- src/no/iterate/Program.java
+++ src/no/iterate/Program.java
@@ -135,8 +135,9 @@ class Program {
         return this.toString();
     }
 
-    public Program peek(){
-        return this;}
+    public Program peek() {
+        return this;
+    }
 
     private static void printLines(String cmd, InputStream ins) throws Exception {
         String line = null;
