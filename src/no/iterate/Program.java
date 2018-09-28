87290c5315e3645c97cc99262adecfc0aafe5009
Lars Barlindhaug <lars.barlindhaug@iterate.no>
2018-09-28 11:34:36 +0200

    working

--- src/no/iterate/Program.java
+++ src/no/iterate/Program.java
@@ -88,6 +88,7 @@ class Program {
 
     public Program addPrimitiveParameter(Primitive parameterType, String parameterName) {
         final PrimitiveType primitiveType = new PrimitiveType(parameterType);
+        new Parameter(primitiveType, parameterName);
         return this;
     }
 
