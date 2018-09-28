b58adf4fcd76019729fa9bbb3a234f82479540c2
oddmunds <oddmunds@iterate.no>
2018-09-28 12:50:42 +0200

    working

--- src/no/iterate/Program.java
+++ src/no/iterate/Program.java
@@ -77,6 +77,7 @@ class Program {
     public void printChildrenRecursively() {
         System.out.println("Nodes:");
         List<Node> nodes = compilationUnit.getChildNodes();
+        printNodes(nodes);
     }
 
     void printNodes(List<Node> nodes){
