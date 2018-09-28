9505f6df7a2012d6f44fa9f2131a34c59a29cd98
oddmunds <oddmunds@iterate.no>
2018-09-28 12:52:30 +0200

    working

--- src/no/iterate/ProgramTests.java
+++ src/no/iterate/ProgramTests.java
@@ -66,7 +66,6 @@ public class ProgramTests {
     public static final Runnable ADD_METHOD_TO_SPECIFIC_CLASS = () -> assume(new Program()
             .addClass("FizzBuzz")
             .addMethodToClass(0, "calculate")
-            .peek(Program::printChildrenRecursively)
             .toString()
             .contains("calculate()"), "Adding a method, 'calculate' should make THE STRING contain 'calculate()'");
 }
